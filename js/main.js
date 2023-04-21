aa43eca4bd8153bfa3080efbc50e0986f8a94908
Andrii <borysenko22@gmail.com>
2023-04-21 15:08:45 +0200

    Added contact

--- js/main.js
+++ js/main.js
@@ -4,6 +4,31 @@ const footerYear = document.querySelector('.footer__year')
 const links = document.querySelectorAll('.nav__link')
 const allSections = document.querySelectorAll('.section')
 
+
+const msgStatus = document.querySelector('.msg-status');
+
+console.log(document.location.search);
+
+if (document.location.search ===  '?mail_status=sent') {
+	msgStatus.classList.add('success');
+	msgStatus.textContent = 'Message sent!';
+
+	setTimeout(() => {
+		msgStatus.classList.remove('success');
+		msgStatus.textContent = '';
+	},3000);
+}
+
+if (document.location.search ===  '?mail_status=error') {
+	msgStatus.classList.add('error');
+	msgStatus.textContent = 'Error occurred.';
+
+	setTimeout(() => {
+		msgStatus.classList.remove('error');
+		msgStatus.textContent = '';
+	},3000);
+}
+
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
---
+++ php/mail.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = $_POST["name"];
+$from = $_POST["email"];
+$subject = "Wiadomość z formularza na stronie.";
+$to = "borusenko93@gmail.com";
+$message = $_POST['msg'];
+
+
+$txt = "Imię: " . $name . "\r\n" . "Email: " . $from . "\r\n" . "\r\n" . "Treść: " . $message;
+
+$headers = "From: " . $from . "\r\n";
+$headers .= "Reply-To: " . $from . "\r\n";
+
+$mail_status = mail($to, $subject, $txt, $headers);
+
+if ($mail_status) {
+    header("Location: /index.html?mail_status=sent");
+}else {
+    header("Location: /index.html?mail_status=error");
+}
